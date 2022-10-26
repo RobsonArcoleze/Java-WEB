95b9180080e401d08d2595a17b04d215ad864aa1
Robson Arcoleze <Robson.arcoleze29@gmail.com>
2022-10-26 18:16:13 -0300

    Subindo repositório

---
+++ JAVA_2/248 - Map/src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+/*		Map<String, String> cookies = new TreeMap<>();
+'		
+'		cookies.put("userName", "Maria"); // ADICIONANDO		
+'		cookies.put("email", "maria@gmail.com");
+'		cookies.put("phone", "99711122");
+'		
+'		cookies.remove("email");
+'		cookies.put("phone", "99711133");
+'		
+'		
+'		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone")); //CONTAINSKEY VERFICA SE CONTÉM A CHAVA PASSADA COMO ARGUMENTO
+'		System.out.println("Phone number: " + cookies.get("phone"));
+'		System.out.println("Email: " + cookies.get("email"));
+'		System.out.println("Size: " + cookies.size());
+'		
+'		System.out.println("ALL COOKIES");
+'		for (String key : cookies.keySet()) {
+'			System.out.println(key + ": " + cookies.get(key));
+*/
+		Map<Product, Double> stock = new HashMap<>();
+		
+		Product p1 = new Product("Tv", 900.0);
+		Product p2 = new Product("Notebook", 1200.0);
+		Product p3 = new Product("Tablet", 400.0);
+		
+		stock.put(p1, 10000.0);
+		stock.put(p2, 20000.0);
+		stock.put(p3, 15000.0);
+		
+		Product ps = new Product("Tv", 900.0);
+		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
+	}
+
+}
