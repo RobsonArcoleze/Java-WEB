0c331293c70476ced1a901ccedc0e912ab4b954e
RobsonDev <robson.arcoleze29@gmail.com>
2022-10-31 16:18:20 -0300

    Subindo atualizações

--- JDBC/loja-virtual-repository/src/TestaListagemDeCategoria.java
+++ JDBC/loja-virtual-repository/src/TestaListagemDeCategoria.java
@@ -3,28 +3,22 @@ import java.sql.SQLException;
 import java.util.List;
 
 import DAO.CategoriaDAO;
-import DAO.ProdutoDAO;
 import modelo.Categoria;
 import modelo.Produto;
 
 public class TestaListagemDeCategoria {
 
 	public static void main(String[] args) throws SQLException {
-		
-		try(Connection connection = new ConnectionFactory().recuperarConexao()){
-			
+
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+
 			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
-			List<Categoria> categoria = categoriaDAO.listar(); 
-			
-			categoria.stream().forEach(ct -> {
+			List<Categoria> categorias = categoriaDAO.listarComProdutos();
+
+			categorias.stream().forEach(ct -> {
 				System.out.println(ct.getNome());
-				try {
-					for(Produto produto : new ProdutoDAO(connection).buscar(ct)) {
-						System.out.println(ct.getNome() + " - " + produto.getNome());
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				for (Produto produto : ct.getProdutos()) {
+					System.out.println(ct.getNome() + " - " + produto.getNome());
 				}
 			});
 		}
--- JDBC/loja-virtual-repository/src/modelo/Categoria.java
+++ JDBC/loja-virtual-repository/src/modelo/Categoria.java
@@ -1,9 +1,13 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
 	
 	private Integer id; 
 	private String nome;
+	private List<Produto> produtos = new ArrayList<>();	
 	
 	public Categoria(Integer id, String nome) {
 
@@ -18,6 +22,16 @@ public class Categoria {
 	public Integer getId() {
 		return id;
 	}
+
+	public void adicionar(Produto produto) {
+		produtos.add(produto);
+		
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
 	
 	
 }
