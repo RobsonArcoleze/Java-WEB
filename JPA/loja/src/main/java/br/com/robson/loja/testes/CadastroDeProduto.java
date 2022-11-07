0751b6b2d6a8b1720821e95fada1f088596fe691
RobsonDev <robson.arcoleze29@gmail.com>
2022-11-07 09:59:27 -0300

    Trabalhando com operações

--- JPA/loja/src/main/java/br/com/robson/loja/testes/CadastroDeProduto.java
+++ JPA/loja/src/main/java/br/com/robson/loja/testes/CadastroDeProduto.java
@@ -23,11 +23,15 @@ public class CadastroDeProduto {
 		
 		em.getTransaction().begin();
 		
-		categoriaDao.cadastrar(celulares);
-		produtoDao.cadastrar(celular);
+		em.persist(celulares);
+		celulares.setNome("XPTO");
 		
-		em.getTransaction().commit();
-		em.close();
+		em.flush();
+		em.clear();
 		
+		celulares = em.merge(celulares);
+		celulares.setNome("Sansumg");
+		em.flush();
+			
 	}
 }
