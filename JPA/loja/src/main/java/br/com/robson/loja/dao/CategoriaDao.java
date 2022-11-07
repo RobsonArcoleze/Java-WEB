8ab7e941f774f0abefebf390917c0bdcef758745
RobsonDev <robson.arcoleze29@gmail.com>
2022-11-07 10:55:21 -0300

    Operação remove

--- JPA/loja/src/main/java/br/com/robson/loja/dao/CategoriaDao.java
+++ JPA/loja/src/main/java/br/com/robson/loja/dao/CategoriaDao.java
@@ -22,4 +22,9 @@ public class CategoriaDao {
 		this.em.merge(categoria); 	
 	}
 
+	public void remover(Categoria categoria) {
+		categoria = em.merge(categoria);
+		this.em.remove(categoria); 	
+	}
+
 }
